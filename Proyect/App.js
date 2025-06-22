31b2dd4bc5885dab7c43cd950bb41612c962fea3
YeseniaGarciaYanez <yesitomate2118@gmail.com>
2025-06-22 07:08:43 -0600

    instalacion de tipografia en App.js

--- Proyect/App.js
+++ Proyect/App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <Text style={{ fontFamily: 'Poppins_700Bold' }}>Demeter en proceso!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +28,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Poppins_500Medium',
+    fontSize: 18,
   },
 });
