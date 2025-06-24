9fc817df3a1b25b9b8dc9f66bf54053ad9aca8be
CitlaliSanchez <0323105963@ut-tijuana.edu.mx>
2025-06-23 20:53:06 -0700

    Update MainTabs.js

--- Proyect/navigation/MainTabs.js
+++ Proyect/navigation/MainTabs.js
@@ -1,12 +1,12 @@
 //archivo donde se accede a los screens, es el menu que tendra los accesos en la parte inferior
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import HomeScreen from '../screens/HomeScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import CropsScreen from '../screens/CropsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
-import AreaCultivos from '../screens/AreaCultivos';
+import ReportScreen from '../screens/ReportScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -21,23 +21,25 @@ export default function MainTabs() {
           let iconName;
 
           if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Profile') {
-            iconName = focused ? 'person' : 'person-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline';
+            iconName = focused ? 'home' : 'home';
+          } else if (route.name === 'Crops') {
+            iconName = focused ? 'seedling' : 'seedling';
+          } else if (route.name === 'Report') {
+            iconName = focused ? 'edit' : 'edit';
+          } else if (route.name === 'Extras') {
+            iconName = focused ? 'bars' : 'bars';
           }
 
-          return <Ionicons name={iconName} size={24} color={color} />;
+          return <FontAwesome5 name={iconName} size={24} color={color} />;
         },
         tabBarActiveTintColor: '#007AFF',
         tabBarInactiveTintColor: 'gray',
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-      <Tab.Screen name="AreaCultivos" component={AreaCultivos} />
+      <Tab.Screen name="Crops" component={CropsScreen} />
+      <Tab.Screen name="Report" component={ReportScreen} />
+      <Tab.Screen name="Extras" component={SettingsScreen} />
     </Tab.Navigator>
   );//cambio de las pantallas, el nombre es el nombre correcto del archivo 
 }
