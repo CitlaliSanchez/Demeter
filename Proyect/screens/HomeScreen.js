62eccc18ec95a83759cb61d5160db37f0d92e378
CitlaliSanchez <0323105963@ut-tijuana.edu.mx>
2025-06-23 20:22:32 -0700

    Add files via upload

--- Proyect/screens/HomeScreen.js
+++ Proyect/screens/HomeScreen.js
@@ -37,10 +37,15 @@ export default function HomeScreen() {
   );
 
   return (
+    
     <SafeAreaView style={styles.container}>
       <Text style={[styles.status, { color: connected ? 'green' : 'red' }]}>
         Estado MQTT: {connected ? 'Conectado' : 'Desconectado'}
       </Text>
+
+      <View style={styles.banner}>
+      <Text>Banner</Text>
+      </View>
 
       <Text style={styles.title}>Últimas Lecturas</Text>
       <FlatList
@@ -57,6 +62,13 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#fff',
+    paddingTop: 55,
+  },
+    banner: {
+    backgroundColor: '#B9F6CA',
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 10,
   },
   status: {
     fontSize: 16,
