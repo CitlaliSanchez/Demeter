3be6927cc03e09dd71537b2f20e1224f74641af1
CitlaliSanchez <0323105963@ut-tijuana.edu.mx>
2025-06-30 19:20:05 -0700

    Add files via upload
    se cambiaron para el login

--- Proyect/navigation/MainDrawer.js
+++ Proyect/navigation/MainDrawer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import { colors, fonts, fontSizes } from '../assets/styles/theme';
 
 import MainTabs from './MainTabs';
@@ -11,32 +10,29 @@ const Drawer = createDrawerNavigator();
 
 export default function MainDrawer() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Inicio"
-        screenOptions={{
-          headerShown: true,
-          drawerActiveTintColor: '#547326',
-          drawerLabelStyle: {
-            fontFamily: 'Poppins_500Medium',
-          },
-        }}
-      >
-        <Drawer.Screen
-          name="Inicio"
-          component={MainTabs}
-          options={{ drawerLabel: 'Pantalla Principal' }}
-        />
-        <Drawer.Screen
-          name="Historial de Mediciones"
-          component={HistoryScreen}
-        />
-        <Drawer.Screen
-          name="Gráficas"
-          component={ChartsScreen}
-        />
-        {/* Aquí agregas más pantallas extras si quieres */}
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Drawer.Navigator
+      initialRouteName="Inicio"
+      screenOptions={{
+        headerShown: true,
+        drawerActiveTintColor: '#547326',
+        drawerLabelStyle: {
+          fontFamily: 'Poppins_500Medium',
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Inicio"
+        component={MainTabs}
+        options={{ drawerLabel: 'Pantalla Principal' }}
+      />
+      <Drawer.Screen
+        name="Historial de Mediciones"
+        component={HistoryScreen}
+      />
+      <Drawer.Screen
+        name="Gráficas"
+        component={ChartsScreen}
+      />
+    </Drawer.Navigator>
   );
 }
